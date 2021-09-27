5f5ae982c5a18fdcc86a89ca72f6a9ce1f312d12
DYLAN MORROW - STUDENT <G00365708@gmit.ie>
2021-09-27 14:37:15 +0100

    Lab Completed

--- bmi.js
+++ bmi.js
@@ -1,16 +1,24 @@
 class BMI
 {
+    // constructor takes in two arguments and sets them to local variables
     constructor(height, weight){
+        // local variable declaration
         this.height = height;
         this.weight = weight;
     }
 
+    // create calculate bmi method
     calculateBMI() {
+        // calculate bmi here
         let bmi = this.weight/(this.height**2); 
+        // return answer
         return bmi;   
     }
 }
 
+// create a bmi object
 let myBMI = new BMI(1.6, 60);
+// invoke the calculate bmi method
 let calc = myBMI.calculateBMI();
+// log calc to console
 console.log(calc);
--- cars.js
+++ cars.js
@@ -1,27 +1,42 @@
 class Vehicle
 {
+    // create constructor for make model and year
     constructor(make, model, year){
+        // assign parameters to local variables
         this.make = make;
         this.model = model;
         this.year = year;
     }
 
+    // create info method to log to console
     Information(){
-        console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+        // log info to console
+        console.log(`Make: ${this.make}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Year: ${this.year}`);
     }
 }
 
+// create a new class that inherits from vehicle
 class Cars extends Vehicle{
+    // create another constructor and add doors
     constructor(make, model, year, doors){
+        // use super to access parent class
         super(make, model, year);
+        // set door to local variable
         this.doors = doors;
     }
 
+    // create another info method
     Information(){
+        // use super to access parent information method
         super.Information();
+        // log doors to console
         console.log(`Doors: ${this.doors}`);
     }
 }
 
+// create a new car object
 let myCar = new Cars('Honda', 'Civic', 2008, 5);
+// send car object info to information method
 myCar.Information();
